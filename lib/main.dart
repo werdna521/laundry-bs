4840a87014239166399ca630a11d45635303ea44
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-02 16:59:40 +0700

    feat: add home widget to scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:app/screens/home.dart';
 
 void main() {
   runApp(App());
@@ -9,28 +9,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          primaryColor: Colors.blue[300],
-        ),
-        home: Scaffold(
-          appBar: AppBar(toolbarHeight: 0.0),
-          backgroundColor: Colors.grey[100],
-          body: Container(
-            padding: EdgeInsets.all(24.0),
-            child: SingleChildScrollView(
-              child: SafeArea(
-                child: Column(
-                  children: [
-                    Container(
-                      width: 48.0,
-                      height: 48.0,
-                      child: Image.asset('assets/logo.png'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ));
+      theme: ThemeData(
+        primaryColor: Colors.blue[300],
+      ),
+      home: HomeScreen(),
+    );
   }
 }
