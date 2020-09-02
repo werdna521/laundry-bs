4165b6631184fb404ab40744e4b2e08e8fb38ecd
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-02 17:32:22 +0700

    feat(card): create base card component widget

---
+++ lib/components/card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AntiCard extends StatelessWidget {
+  final child;
+
+  const AntiCard({@required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(2.0, 4.0),
+            blurRadius: 8.0,
+          )
+        ],
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+      child: child,
+    );
+  }
+}
