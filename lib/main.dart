1b36c54584ffd47eb9fba26711459c5a9004cea5
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-02 19:10:42 +0700

    feat(app): add custom theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:app/screens/home.dart';
 
+final kTheme = ThemeData(
+  primaryColor: Colors.blue[300],
+  fontFamily: 'Rubik',
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      fontSize: 36.0,
+      color: Colors.grey[850],
+    ),
+    headline6: TextStyle(
+      fontSize: 14.0,
+      color: Colors.grey[850],
+    ),
+  ),
+);
+
 void main() {
   runApp(App());
 }
@@ -9,9 +24,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue[300],
-      ),
+      theme: kTheme,
       home: HomeScreen(),
     );
   }
