cea82f0e704bc18e162b6afaa5466fe3114840bd
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-02 20:49:17 +0700

    feat(counter_card): create counter card

---
+++ lib/components/counter_card.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:app/text_theme.dart';
+import 'package:app/components/card.dart';
+
+class CounterCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AntiCard(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Counter 1', style: AntiTheme.headline6(context)),
+          SizedBox(height: 4.0),
+          Text('1', style: AntiTheme.headline1(context)),
+        ],
+      ),
+    );
+  }
+}
