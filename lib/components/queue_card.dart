9a2aedf9cb431b4d2fe98e4e76cd13a546132a68
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-02 19:11:16 +0700

    feat: update queue card

--- lib/components/queue_card.dart
+++ lib/components/queue_card.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:app/components/card.dart';
+import 'package:app/text_theme.dart';
 
 class QueueCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AntiCard(
-        child: Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        QueueNumberIndicator(),
-        Container(
-          height: 20.0,
-          child: VerticalDivider(
-            color: Colors.grey[850],
-            width: 16.0,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          QueueNumberIndicator(
+            title: 'Your Queue No.',
+            content: '10',
           ),
-        ),
-        QueueNumberIndicator(),
-      ],
-    ));
+          Container(
+            height: 48.0,
+            child: VerticalDivider(
+              color: Colors.grey[850],
+              width: 8.0,
+            ),
+          ),
+          QueueNumberIndicator(
+            title: 'Current Queue No.',
+            content: '2',
+          ),
+        ],
+      ),
+    );
   }
 }
 
 class QueueNumberIndicator extends StatelessWidget {
+  final title;
+  final content;
+
+  const QueueNumberIndicator({@required this.title, @required this.content});
+
   @override
   Widget build(BuildContext context) {
     return Flexible(
@@ -31,8 +44,14 @@ class QueueNumberIndicator extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('Queue No'),
-          Text('5'),
+          Text(
+            title,
+            style: AntiTheme.headline6(context),
+          ),
+          Text(
+            content,
+            style: AntiTheme.headline1(context),
+          ),
         ],
       ),
     );
