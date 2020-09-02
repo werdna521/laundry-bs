c125ccef0f5f024cf4798615f96d17432a449519
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-02 16:50:21 +0700

    fix: remove system chrome tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,6 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: Color(0xFF880E4F)));
     return MaterialApp(
         theme: ThemeData(
           primaryColor: Colors.blue[300],
