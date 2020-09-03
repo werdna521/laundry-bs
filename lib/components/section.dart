ea002762b47191a1f5b0bda1a613a408e797b2d8
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-03 22:02:04 +0700

    feat(section): update section compoennt

--- lib/components/section.dart
+++ lib/components/section.dart
@@ -1,11 +1,37 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:app/text_theme.dart';
 
 class Section extends StatelessWidget {
   final title;
   final child;
+  final buttonText;
+  final onPress;
 
-  const Section({@required this.title, @required this.child});
+  const Section({
+    @required this.title,
+    @required this.child,
+    this.buttonText,
+    this.onPress,
+  });
+
+  List<Widget> _buildRow(BuildContext context) {
+    final List<Widget> builder = [
+      Text(
+        title,
+        style: AntiTheme.headline2(context),
+      ),
+    ];
+
+    if (buttonText != null)
+      builder.add(FlatButton(
+        textColor: Colors.blue[300],
+        onPressed: onPress,
+        child: Text(buttonText),
+      ));
+
+    return builder;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +40,11 @@ class Section extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.only(left: 24.0),
-            child: Text(
-              title,
-              style: AntiTheme.headline2(context),
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: _buildRow(context),
             ),
           ),
           Container(child: child),
