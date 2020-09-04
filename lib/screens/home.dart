2fc63d31f0fdeec3f18b3c04cea6cc7e027b71a0
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-09-04 18:40:12 +0700

    test(home): implement test code for see more

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -18,94 +18,97 @@ class HomeScreen extends StatelessWidget {
         onTap: () {},
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 24.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24.0),
-                  child: Column(
+      body: Builder(
+        builder: (context) => SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 48.0,
+                          height: 48.0,
+                          child: Image.asset('assets/images/logo.png'),
+                        ),
+                        SizedBox(height: 24.0),
+                        QueueCard(),
+                        SizedBox(height: 24.0),
+                      ],
+                    ),
+                  ),
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        width: 48.0,
-                        height: 48.0,
-                        child: Image.asset('assets/images/logo.png'),
-                      ),
-                      SizedBox(height: 24.0),
-                      QueueCard(),
-                      SizedBox(height: 24.0),
-                    ],
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Section(
-                      title: 'Queue',
-                      child: Container(
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: 108.0,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, index) {
-                              if (index == 0 || index == 10)
-                                return SizedBox(
-                                  width: 24.0,
-                                );
+                      Section(
+                        title: 'Queue',
+                        child: Container(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: 108.0,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                if (index == 0 || index == 10)
+                                  return SizedBox(
+                                    width: 24.0,
+                                  );
 
-                              if (index % 2 == 0)
-                                return SizedBox(
-                                  width: 16.0,
-                                );
+                                if (index % 2 == 0)
+                                  return SizedBox(
+                                    width: 16.0,
+                                  );
 
-                              return Padding(
-                                padding: EdgeInsets.symmetric(vertical: 8.0),
-                                child: CounterCard(
-                                  counterNumber: (index / 2).ceil(),
-                                  counterQueueNumber:
-                                      Random().nextInt(100).toString(),
-                                ),
-                              );
-                            },
-                            itemCount: 11,
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                                  child: CounterCard(
+                                    counterNumber: (index / 2).ceil(),
+                                    counterQueueNumber:
+                                        Random().nextInt(100).toString(),
+                                  ),
+                                );
+                              },
+                              itemCount: 11,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Section(
-                      title: 'History',
-                      buttonText: 'SEE MORE',
-                      onPress: () {},
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 24.0),
-                        child: Column(
-                          children: [
-                            HistoryCard(
-                              history: History(
-                                'August 12, 2020',
-                                false,
+                      Section(
+                        title: 'History',
+                        buttonText: 'SEE MORE',
+                        onPress: () => Scaffold.of(context).showSnackBar(
+                            SnackBar(content: const Text('See More'))),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 24.0),
+                          child: Column(
+                            children: [
+                              HistoryCard(
+                                history: History(
+                                  'August 12, 2020',
+                                  false,
+                                ),
                               ),
-                            ),
-                            SizedBox(height: 8.0),
-                            HistoryCard(
-                              history: History(
-                                'August 1, 2020',
-                                true,
-                                'August 9, 2020',
+                              SizedBox(height: 8.0),
+                              HistoryCard(
+                                history: History(
+                                  'August 1, 2020',
+                                  true,
+                                  'August 9, 2020',
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
